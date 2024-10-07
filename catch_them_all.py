f806c258db32e2cb666c03576d3fac0d85f4ff0e
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-07 15:38:20 +0200

    Refactor variable names and add .gitignore
    Renamed variables for consistency and readability. Updated function definitions and comments to reflect the name changes. Added a .gitignore file to exclude IDE-specific folders.

--- catch_them_all.py
+++ catch_them_all.py
@@ -1,11 +1,10 @@
 import requests
 import random
 
-URL_POKEMON_API_BASE = "https://pokeapi.co/api/v2/pokemon"
-NB_PARTICIPANTS = 16
+BASE_URL_POKEMON_API = "https://pokeapi.co/api/v2/pokemon"
+NUMBER_OF_PARTICIPANTS = 16
 
-# Définition des avantages entre types de Pokémon
-type_advantages = {
+TYPE_ADVANTAGES = {
     "normal": [],
     "fire": ["grass", "bug", "ice", "steel"],
     "water": ["fire", "ground", "rock"],
@@ -26,101 +25,63 @@ type_advantages = {
     "fairy": ["fighting", "dragon", "dark"]
 }
 
-def get_pokemons_count():
-    """
-    Make a GET request to the Pokémon API to retrieve the total count of Pokémon.
 
-    :return: The total number of Pokémon as an integer. If an error occurs
-             during the request, it returns 0.
-    """
+def get_total_pokemons():
     try:
-        response = requests.get(URL_POKEMON_API_BASE)
+        response = requests.get(BASE_URL_POKEMON_API)
         response.raise_for_status()
-        data = response.json()
-        return data["count"]
+        return response.json()["count"]
     except requests.RequestException:
         print("Erreur lors de la requête pour récupération du nombre total de pokémons")
         exit()
 
-def get_random_pokemon_id(pokemons_count):
-    """
-    :param pokemons_count: The total number of available Pokémon.
-    :return: A random Pokémon ID between 1 and the total number of available Pokémon.
-    """
-    return random.randint(1, pokemons_count)
+
+def get_random_pokemon_id(pokemon_count):
+    return random.randint(1, pokemon_count)
+
 
 def fetch_pokemon_data(pokemon_id):
-    """
-    :param pokemon_id: The ID of the Pokemon to fetch data for.
-    :return: A dictionary containing the Pokemon data if the request is successful; None if there is a request error.
-    """
     try:
-        url_pokemon = f"{URL_POKEMON_API_BASE}/{pokemon_id}"
-        response = requests.get(url_pokemon)
+        url = f"{BASE_URL_POKEMON_API}/{pokemon_id}"
+        response = requests.get(url)
         response.raise_for_status()
         return response.json()
     except requests.RequestException:
         return None
 
-def get_random_pokemons():
-    """
-    Fetches a list of random Pokemon data. The number of Pokemon is defined by
-    the constant NB_PARTICIPANTS. Ensures no duplicates are added to the list.
 
-    :return: A list of random Pokemon data.
-    """
+def get_random_pokemons():
     pokemons = []
-    pokemons_count = get_pokemons_count()
-    while len(pokemons) < NB_PARTICIPANTS:
-        pokemon_id = get_random_pokemon_id(pokemons_count)
+    pokemon_count = get_total_pokemons()
+    while len(pokemons) < NUMBER_OF_PARTICIPANTS:
+        pokemon_id = get_random_pokemon_id(pokemon_count)
         pokemon_data = fetch_pokemon_data(pokemon_id)
         if pokemon_data and pokemon_data not in pokemons:
             pokemons.append(pokemon_data)
     return pokemons
 
+
 def calculate_pokemon_strength(pokemon):
-    """
-    :param pokemon: A dictionary representing a Pokémon, which includes its stats.
-    :type pokemon: dict
-    :return: The total strength of the Pokémon calculated as the sum of its base stats.
-    :rtype: int
-    """
-    # On calcule la force d'un pokémon comme la somme de ses statistiques de base
-    stats = pokemon['stats']
-    total_strength = sum(stat['base_stat'] for stat in stats)
-    return total_strength
+    return sum(stat['base_stat'] for stat in pokemon['stats'])
+
 
 def get_type_advantage_multiplier(pokemon1, pokemon2):
-    """
-    :param pokemon1: Dictionary representing the first Pokémon with its respective types.
-    :param pokemon2: Dictionary representing the second Pokémon with its respective types.
-    :return: A float representing the type advantage multiplier between the two Pokémon.
-    """
-    # On vérifie les avantages de type entre deux pokémons
     types1 = [t['type']['name'] for t in pokemon1['types']]
     types2 = [t['type']['name'] for t in pokemon2['types']]
     multiplier = 1.0
     for t1 in types1:
-        if t1 in type_advantages:
+        if t1 in TYPE_ADVANTAGES:
             for t2 in types2:
-                if t2 in type_advantages[t1]:
-                    multiplier *= 1.5  # Avantage de type
+                if t2 in TYPE_ADVANTAGES[t1]:
+                    multiplier *= 1.5
     return multiplier
 
+
 def simulate_battle(pokemon1, pokemon2):
-    """
-    :param pokemon1: Dictionary containing attributes of the first Pokémon.
-    :param pokemon2: Dictionary containing attributes of the second Pokémon.
-    :return: Dictionary containing attributes of the winning Pokémon.
-    """
-    # On calcule la force de chaque pokémon
     strength1 = calculate_pokemon_strength(pokemon1)
     strength2 = calculate_pokemon_strength(pokemon2)
-
-    # On applique les avantages de type
     multiplier1 = get_type_advantage_multiplier(pokemon1, pokemon2)
     multiplier2 = get_type_advantage_multiplier(pokemon2, pokemon1)
-
     adjusted_strength1 = strength1 * multiplier1
     adjusted_strength2 = strength2 * multiplier2
 
@@ -128,7 +89,6 @@ def simulate_battle(pokemon1, pokemon2):
     print(f" - {pokemon1['name']} : Force totale = {strength1} (après avantage de type : {adjusted_strength1})")
     print(f" - {pokemon2['name']} : Force totale = {strength2} (après avantage de type : {adjusted_strength2})")
 
-    # On compare les forces pour déterminer le vainqueur
     if adjusted_strength1 > adjusted_strength2:
         print(f" --> Vainqueur : {pokemon1['name']}\n")
         return pokemon1
@@ -136,49 +96,36 @@ def simulate_battle(pokemon1, pokemon2):
         print(f" --> Vainqueur : {pokemon2['name']}\n")
         return pokemon2
     else:
-        # En cas d'égalité parfaite, on choisit un vainqueur aléatoire
         winner = random.choice([pokemon1, pokemon2])
         print(f" --> Égalité parfaite, vainqueur aléatoire : {winner['name']}\n")
         return winner
 
+
 def simulate_round(pokemons):
-    """
-    :param pokemons: List of Pokémon objects participating in the round.
-    :return: List of Pokémon objects that won their respective battles.
-    """
-    # On simule un round de combats
     winners = []
     for i in range(0, len(pokemons), 2):
         winner = simulate_battle(pokemons[i], pokemons[i + 1])
         winners.append(winner)
     return winners
 
-def main():
-    """
-    Main function to run a Pokémon tournament.
-    Ensures the number of participants is a power of 2 and at least 2.
-    Randomly selects Pokémon, prints them, and simulates tournament rounds until a champion is declared.
 
-    :return: None
-    """
-    if NB_PARTICIPANTS < 2 or (NB_PARTICIPANTS & (NB_PARTICIPANTS - 1)) != 0:
+def main():
+    if NUMBER_OF_PARTICIPANTS < 2 or (NUMBER_OF_PARTICIPANTS & (NUMBER_OF_PARTICIPANTS - 1)) != 0:
         print("Le nombre de participants doit être une puissance de 2 et supérieur ou égal à 2.")
         return
-
     random_pokemons = get_random_pokemons()
-    print(f"Les {NB_PARTICIPANTS} Pokémon choisis aléatoirement sont :")
+    print(f"Les {NUMBER_OF_PARTICIPANTS} Pokémon choisis aléatoirement sont :")
     for i, pokemon in enumerate(random_pokemons, start=1):
         print(f"{i}. {pokemon['name']}")
-    
     round_number = 1
     current_pokemons = random_pokemons
     while len(current_pokemons) > 1:
         print(f"\n--- Tour {round_number} ---\n")
         current_pokemons = simulate_round(current_pokemons)
         round_number += 1
-    
     print("\nLe champion est :")
     print(f"{current_pokemons[0]['name']}")
+
 
 if __name__ == "__main__":
     main()
